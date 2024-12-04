5df436ec85c5e3800219b2fec7c85c8669b923b9
Juan <Juan@DESKTOP-9URPS09>
2024-12-04 01:23:08 -0500

    Implementacion de iconos y codigo de hover de nuevos botones

--- src/Presentacion/interfazPrincipal.java
+++ src/Presentacion/interfazPrincipal.java
@@ -18,9 +18,10 @@ public class interfazPrincipal extends javax.swing.JFrame {
         opciones = new javax.swing.JPanel();
         btnInicio = new javax.swing.JButton();
         btnProveedor = new javax.swing.JButton();
-        btnCategoria = new javax.swing.JButton();
         btnUsuarios = new javax.swing.JButton();
         btnProducto = new javax.swing.JButton();
+        btnTransaciones = new javax.swing.JButton();
+        btnLaboratorio = new javax.swing.JButton();
         content = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -69,8 +70,8 @@ public class interfazPrincipal extends javax.swing.JFrame {
         btnProveedor.setBackground(new java.awt.Color(25, 80, 180));
         btnProveedor.setFont(new java.awt.Font("SansSerif", 1, 14)); // NOI18N
         btnProveedor.setForeground(new java.awt.Color(255, 255, 255));
-        btnProveedor.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/inicio.png"))); // NOI18N
-        btnProveedor.setText("PROVEEDOR");
+        btnProveedor.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/proveedor.png"))); // NOI18N
+        btnProveedor.setText("PROVEEDORES");
         btnProveedor.setBorder(javax.swing.BorderFactory.createMatteBorder(1, 10, 1, 1, new javax.swing.ImageIcon(getClass().getResource("/Imagenes/inicio.png")))); // NOI18N
         btnProveedor.setBorderPainted(false);
         btnProveedor.setContentAreaFilled(false);
@@ -78,19 +79,11 @@ public class interfazPrincipal extends javax.swing.JFrame {
         btnProveedor.setFocusPainted(false);
         btnProveedor.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
         btnProveedor.setIconTextGap(10);
-
-        btnCategoria.setBackground(new java.awt.Color(25, 80, 180));
-        btnCategoria.setFont(new java.awt.Font("SansSerif", 1, 14)); // NOI18N
-        btnCategoria.setForeground(new java.awt.Color(255, 255, 255));
-        btnCategoria.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/inicio.png"))); // NOI18N
-        btnCategoria.setText("CATEGORIA");
-        btnCategoria.setBorder(javax.swing.BorderFactory.createMatteBorder(1, 10, 1, 1, new java.awt.Color(0, 0, 0)));
-        btnCategoria.setBorderPainted(false);
-        btnCategoria.setContentAreaFilled(false);
-        btnCategoria.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        btnCategoria.setFocusPainted(false);
-        btnCategoria.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
-        btnCategoria.setIconTextGap(10);
+        btnProveedor.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnProveedorActionPerformed(evt);
+            }
+        });
 
         btnUsuarios.setBackground(new java.awt.Color(25, 80, 180));
         btnUsuarios.setFont(new java.awt.Font("SansSerif", 1, 14)); // NOI18N
@@ -118,8 +111,8 @@ public class interfazPrincipal extends javax.swing.JFrame {
         btnProducto.setBackground(new java.awt.Color(25, 80, 180));
         btnProducto.setFont(new java.awt.Font("SansSerif", 1, 14)); // NOI18N
         btnProducto.setForeground(new java.awt.Color(255, 255, 255));
-        btnProducto.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/inicio.png"))); // NOI18N
-        btnProducto.setText("PRODUCTO");
+        btnProducto.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/producto.png"))); // NOI18N
+        btnProducto.setText("INVENTARIO");
         btnProducto.setBorder(javax.swing.BorderFactory.createMatteBorder(1, 10, 1, 1, new java.awt.Color(0, 0, 0)));
         btnProducto.setBorderPainted(false);
         btnProducto.setContentAreaFilled(false);
@@ -129,15 +122,52 @@ public class interfazPrincipal extends javax.swing.JFrame {
         btnProducto.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
         btnProducto.setIconTextGap(10);
 
+        btnTransaciones.setBackground(new java.awt.Color(25, 80, 180));
+        btnTransaciones.setFont(new java.awt.Font("SansSerif", 1, 14)); // NOI18N
+        btnTransaciones.setForeground(new java.awt.Color(255, 255, 255));
+        btnTransaciones.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/transaccion.png"))); // NOI18N
+        btnTransaciones.setText("TRANSACCIONES");
+        btnTransaciones.setBorder(javax.swing.BorderFactory.createMatteBorder(1, 10, 1, 1, new javax.swing.ImageIcon(getClass().getResource("/Imagenes/inicio.png")))); // NOI18N
+        btnTransaciones.setBorderPainted(false);
+        btnTransaciones.setContentAreaFilled(false);
+        btnTransaciones.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btnTransaciones.setFocusPainted(false);
+        btnTransaciones.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        btnTransaciones.setIconTextGap(10);
+        btnTransaciones.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnTransacionesActionPerformed(evt);
+            }
+        });
+
+        btnLaboratorio.setBackground(new java.awt.Color(25, 80, 180));
+        btnLaboratorio.setFont(new java.awt.Font("SansSerif", 1, 14)); // NOI18N
+        btnLaboratorio.setForeground(new java.awt.Color(255, 255, 255));
+        btnLaboratorio.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/laboratorio.png"))); // NOI18N
+        btnLaboratorio.setText("LABORATORIOS");
+        btnLaboratorio.setBorder(javax.swing.BorderFactory.createMatteBorder(1, 10, 1, 1, new javax.swing.ImageIcon(getClass().getResource("/Imagenes/inicio.png")))); // NOI18N
+        btnLaboratorio.setBorderPainted(false);
+        btnLaboratorio.setContentAreaFilled(false);
+        btnLaboratorio.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btnLaboratorio.setFocusPainted(false);
+        btnLaboratorio.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        btnLaboratorio.setIconTextGap(10);
+        btnLaboratorio.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnLaboratorioActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout opcionesLayout = new javax.swing.GroupLayout(opciones);
         opciones.setLayout(opcionesLayout);
         opcionesLayout.setHorizontalGroup(
             opcionesLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(btnInicio, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addComponent(btnUsuarios, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .addComponent(btnProveedor, javax.swing.GroupLayout.DEFAULT_SIZE, 190, Short.MAX_VALUE)
-            .addComponent(btnCategoria, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addComponent(btnProducto, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(btnProveedor, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(btnLaboratorio, javax.swing.GroupLayout.DEFAULT_SIZE, 190, Short.MAX_VALUE)
+            .addComponent(btnTransaciones, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         opcionesLayout.setVerticalGroup(
             opcionesLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -146,12 +176,14 @@ public class interfazPrincipal extends javax.swing.JFrame {
                 .addGap(0, 0, 0)
                 .addComponent(btnUsuarios, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(0, 0, 0)
+                .addComponent(btnProducto, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, 0)
                 .addComponent(btnProveedor, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(0, 0, 0)
-                .addComponent(btnCategoria, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(btnLaboratorio, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(0, 0, 0)
-                .addComponent(btnProducto, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(256, Short.MAX_VALUE))
+                .addComponent(btnTransaciones, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(206, Short.MAX_VALUE))
         );
 
         bg.add(opciones, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 100, 190, 500));
@@ -173,7 +205,7 @@ public class interfazPrincipal extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(bg, javax.swing.GroupLayout.DEFAULT_SIZE, 1050, Short.MAX_VALUE)
+            .addComponent(bg, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -199,14 +231,27 @@ public class interfazPrincipal extends javax.swing.JFrame {
         
     }//GEN-LAST:event_btnUsuariosMouseEntered
 
+    private void btnProveedorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnProveedorActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnProveedorActionPerformed
+
+    private void btnTransacionesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnTransacionesActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnTransacionesActionPerformed
+
+    private void btnLaboratorioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLaboratorioActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnLaboratorioActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel banner;
     private javax.swing.JPanel bg;
-    private javax.swing.JButton btnCategoria;
     private javax.swing.JButton btnInicio;
+    private javax.swing.JButton btnLaboratorio;
     private javax.swing.JButton btnProducto;
     private javax.swing.JButton btnProveedor;
+    private javax.swing.JButton btnTransaciones;
     private javax.swing.JButton btnUsuarios;
     private javax.swing.JPanel content;
     private javax.swing.JLabel jLabel1;
@@ -256,22 +301,6 @@ public class interfazPrincipal extends javax.swing.JFrame {
             btnProveedor.setBackground(new java.awt.Color(25, 80, 180)); // Color original
         }
     });
-        
-        //Boton Categoria
-        
-        btnCategoria.addMouseListener(new java.awt.event.MouseAdapter() {
-        @Override
-        public void mouseEntered(java.awt.event.MouseEvent evt) {
-            btnCategoria.setBackground(new java.awt.Color(50, 150, 255)); // Color al pasar el mouse
-            btnCategoria.setOpaque(true); // Asegura que se vea el cambio de color
-        }
-
-        @Override
-        public void mouseExited(java.awt.event.MouseEvent evt) {
-            btnCategoria.setBackground(new java.awt.Color(25, 80, 180)); // Color original
-        }
-    });
-        
         //Boton usuario
         btnUsuarios.addMouseListener(new java.awt.event.MouseAdapter() {
         @Override
@@ -285,10 +314,51 @@ public class interfazPrincipal extends javax.swing.JFrame {
             btnUsuarios.setBackground(new java.awt.Color(25, 80, 180)); // Color original
         }
     });
-       
+       //Boton Laboratorio
+        
+        btnLaboratorio.addMouseListener(new java.awt.event.MouseAdapter() {
+        @Override
+        public void mouseEntered(java.awt.event.MouseEvent evt) {
+            btnLaboratorio.setBackground(new java.awt.Color(50, 150, 255)); // Color al pasar el mouse
+            btnLaboratorio.setOpaque(true); // Asegura que se vea el cambio de color
+        }
 
+        @Override
+        public void mouseExited(java.awt.event.MouseEvent evt) {
+            btnLaboratorio.setBackground(new java.awt.Color(25, 80, 180)); // Color original
+            
+        }
+    });
+        //Boton proveedores
+        
+        btnProveedor.addMouseListener(new java.awt.event.MouseAdapter() {
+        @Override
+        public void mouseEntered(java.awt.event.MouseEvent evt) {
+            btnProveedor.setBackground(new java.awt.Color(50, 150, 255)); // Color al pasar el mouse
+            btnProveedor.setOpaque(true); // Asegura que se vea el cambio de color
+        }
+
+        @Override
+        public void mouseExited(java.awt.event.MouseEvent evt) {
+            btnProveedor.setBackground(new java.awt.Color(25, 80, 180)); // Color original
+        }
+    });
     
-    
+        
+        //Boton Transaciones
+        
+        btnTransaciones.addMouseListener(new java.awt.event.MouseAdapter() {
+        @Override
+        public void mouseEntered(java.awt.event.MouseEvent evt) {
+            btnTransaciones.setBackground(new java.awt.Color(50, 150, 255)); // Color al pasar el mouse
+            btnTransaciones.setOpaque(true); // Asegura que se vea el cambio de color
+        }
+
+        @Override
+        public void mouseExited(java.awt.event.MouseEvent evt) {
+            btnTransaciones.setBackground(new java.awt.Color(25, 80, 180)); // Color original
+        }
+    });
     
     
     }
