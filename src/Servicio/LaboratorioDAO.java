f052a889e51812b7f7fb383e94a00f3d1d84a408
VictorSogamosoWW <victor.sogamoso.05@gmail.com>
2024-12-09 18:18:33 -0500

    laboratorio dao

---
+++ src/Servicio/LaboratorioDAO.java
@@ -0,0 +1,22 @@
+package Servicio;
+
+public class LaboratorioDAO {
+    private int codigoLaboratorio;
+    private String nombreLaboratorio;
+
+    public int getCodigoLaboratorio() {
+        return codigoLaboratorio;
+    }
+
+    public void setCodigoLaboratorio(int codigoLaboratorio) {
+        this.codigoLaboratorio = codigoLaboratorio;
+    }
+
+    public String getNombreLaboratorio() {
+        return nombreLaboratorio;
+    }
+
+    public void setNombreLaboratorio(String nombreLaboratorio) {
+        this.nombreLaboratorio = nombreLaboratorio;
+    }
+}
